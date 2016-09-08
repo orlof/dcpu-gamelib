0f4a87efa35d1cd686125a52c49e493a47952d52
Orlof <orlof@users.noreply.github.com>
2016-09-08 18:24:36 +0300

    add comments

--- scripts/image_convert.sh
+++ scripts/image_convert.sh
@@ -1,1 +1,6 @@
-convert in.jpg -crop 128x96+65+110 -resize 64x48! -dither none -colors 16 out.png
+#!/usr/bin/env bash
+
+# this is only an example
+# edit crop area
+
+convert $1 -crop 128x96+65+110 -resize 64x48! -dither none -colors 16 $2
--- scripts/image_convert2.sh
+++ scripts/image_convert2.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+
+# this is only an example
+
 python lib/image_converter.py images/logo.png images/kermit.png images/piggy.png images/animal.png muppet.dat
