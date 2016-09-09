e64d5571c80c11a386fcac58da13414c808b8342
Orlof <orlof@users.noreply.github.com>
2016-09-09 13:27:49 +0300

    changed python converter name

--- scripts/image_convert2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# this is only an example
-
-python lib/image_converter.py images/logo.png images/kermit.png images/piggy.png images/animal.png muppet.dat
---
+++ scripts/lem_converter.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# this is only an example
+
+python lib/lem_converter.py images/logo.png images/kermit.png images/piggy.png images/animal.png muppet.dat
